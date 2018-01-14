cd367590991822ce3e4445714094be0d8de4cd3f
Julian Johannesen <julian.johannesen@gmail.com>
2018-01-14 18:03:44 -0500

    Moved some variables to the top and some other stuff

--- scripts.js
+++ scripts.js
@@ -1,14 +1,15 @@
 const apiKey = "5d576382955ff5829fc3844390db4427";
 const baseAPIUrl = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&sort_by=popularity.desc`;
 const posterBaseUrl = "https://image.tmdb.org/t/p/w500";
+let genreCode, theYear, posterPath, imgUrl
 
 $(function () {  
   function afterGoClicked() {
-    let genreCode, theYear;
     genreCode = "with_genres=" + $("#genre option:selected").val();
     if ($("#year").val() >= 1900 && $("#year").val() <= 2020) {
       theYear = "primary_release_year=" + $("#year").val();
     } else {
+      theYear = 0;
       $("p.validity").html("Please enter a valid 4 digit year between 1900 and 2020.");
     }
     const theFullUrl = buildQueryString(baseAPIUrl, genreCode, theYear);
@@ -18,14 +19,14 @@ $(function () {
     return baseUrl + "&" + genre + "&" + year;
   }
   function afterDataLoaded(dataObject){
-    let posterPath, imgUrl;
     Array.prototype.map.call(document.querySelectorAll("img"), (val,i) => {
         posterPath = dataObject.results[i].poster_path;
-        if(posterPath){
+        if(posterPath && theYear){
           imgUrl = posterBaseUrl + posterPath;
         } else {
           imgUrl = "img/placeholder.jpg"
         }
+        document.getElementById("#movies").style.display = "block";
         val.setAttribute("src", imgUrl);
       });
   }
